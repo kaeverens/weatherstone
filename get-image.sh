c94349e512abc66fbf71812c21b7090afe43ac04
Kae Verens <kae.verens@gmail.com>
2021-05-23 14:48:23 +0000

    add classification buttons

--- get-image.sh
+++ get-image.sh
@@ -5,7 +5,7 @@ FS_CONT=0
 FNAME=www/images/`date +"%Y%m%d%H%M%S"`.png
 
 fswebcam --quiet -d v4l2:/dev/video0 --no-banner \
-	-F 5 -S 200 --png 9 $dname \
+	-F 1 -S 200 --png 9 $dname \
 	-r "640x480" \
 	--set brightness=${FS_BRIGHT}% --set contrast=${FS_CONT} \
 	--save ${FNAME}
--- www/js.js
+++ www/js.js
@@ -1,4 +1,5 @@
-let $img, net;
+let $img, net, classifier, classNames = ['Overcast', 'Sunny', 'Raining', 'Breezy'];
+classifier = knnClassifier.create();
 async function app() {
 	function getNextImage() {
 		$.post('/php/image-get.php', r=>{
@@ -6,28 +7,45 @@ async function app() {
 			$img.attr('src', r.url);
 		});
 	}
-	function showResults(results) {
-	console.log(results);
+	function showResults(result) {
 		var $table=$('<table class="results"><tbody/></table>');
-		results.forEach(result=>{
+		Object.keys(result.confidences).forEach(k=>{
 			var $tr=$('<tr><td class="confidence"/><td class="label"/></tr>');
-			$tr.find('.confidence').text((result.probability*100).toFixed(2)+'%');
-			$tr.find('.label').text(result.className);
+			$tr.find('.confidence').text((result.confidences[k]*100).toFixed(2)+'%');
+			$tr.find('.label').text(k);
 			$tr.appendTo($table.find('tbody'));
 		});
 		$('#results').empty().append($table);
 	}
 	async function classify() {
-		const results=await net.classify($img[0]);
-		showResults(results);
-		setTimeout(getNextImage, 60000);
+    if (classifier.getNumClasses() > 0) {
+      const img = $img[0];
+
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(img, 'conv_preds');
+      // Get the most likely class and confidence from the classifier module.
+      const results = await classifier.predictClass(activation);
+			showResults(results);
+      // Dispose the tensor to release the memory.
+    }
+		setTimeout(getNextImage, 10000);
 	}
 	$img=$('#img');
 	$img.on('load', classify);
 	net=await mobilenet.load();
 	getNextImage();
+	const $classifiers=$('#classifiers')
+		.on('click', 'button', function() {
+			addExample($(this).text());
+		});
+	for (let i=0;i<classNames.length;++i) {
+		$('<button data-idx="'+i+'"/>').text(classNames[i]).appendTo($classifiers);
+	}
+	async function addExample(classId) {
+		const activation = net.infer($img[0], true);
+		classifier.addExample(activation, classId);
+	}
 }
-
 $(()=>{
 	app();
 });
